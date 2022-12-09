33f3eef7c9440ca38718d7f1aba4df70fb15a35c
JuanLicciardi <juanelicciardi@gmail.com>
2022-12-08 22:16:40 -0300

    Microdesafio 02 // Finalizado

---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CardGift from './CardGift'
+
+
+function Home() {
+  return (
+  
+  <div className="container">
+
+    <CardGift/>
+   
+
+
+  </div>
+
+  )
+}
+
+export default Home
