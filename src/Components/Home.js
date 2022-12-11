19482c7dbdb806da50a11cad7b223d158dfdf0b0
JuanLicciardi <juanelicciardi@gmail.com>
2022-12-11 10:26:02 -0300

    Finalizado micro Desafio 4

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,36 +4,58 @@ import CardGift from './CardGift'
 
 class Home extends Component {
     
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            gif: ""
+            gifs: [] ,
+            limit: 8,
         }
     }    
     
+
     apiCall(url){
         fetch(url)
             .then (response => response.json())
-            .then (data => console.log(data))
+            .then (data => {
+                this.setState({
+                    gifs : data.data
+                })
+                console.log("---------------",data)
+                }).catch(error => console.log(error))
     }    
     
+
     componentDidMount() {
         console.log("El componente fue renderizado")
-        this.apiCall("https://api.giphy.com/v1/gifs/trending?api_key=aIfxklgxHlgGRU3K4gr28KOoRP0G8ohq&limit=1&rating=g")
+        this.apiCall(`https://api.giphy.com/v1/gifs/trending?api_key=aIfxklgxHlgGRU3K4gr28KOoRP0G8ohq&limit=${this.state.limit}&rating=g`)
     }
 
     componentDidUpdate() {
-        console.log("El componente se ha actualizado ")
         
+        console.log("El componente se ha actualizado ")
+       
     }
-    
+
+
     render (){
+        
         return (
             <div className="container">
-
-                <CardGift/>
+                {this.state.gifs.length === 0 && <p>Cargando....</p>}
+                <div className="row text-center">
+                    {
+                        this.state.gifs.map((gif,index)=>{
+                            return (
+                                <div> 
+                                    <CardGift key={index} {...gif}     />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
-    )}
+
+)}
 
 
 
